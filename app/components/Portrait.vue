c4f98f5bb4dac663be948d5cf3fca568595be023
puntodamar <punto.damar2@gmail.com>
2025-10-03 00:43:59 +0700

    create: portrait component

---
+++ app/components/Portrait.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="pointer-events-none hidden md:block absolute inset-y-0 right-0 w-1/2">
+    <div class="h-full hidden lg:block bg-gray-800 [clip-path:polygon(18%_0,100%_0,100%_100%,0_100%)]">
+      <div class="absolute  bottom-0 xl:top-20 lg:flex lg:items-center lg:justify-center">
+        <img
+            src="/photo-full-downsized.png"
+            alt="portrait"
+            class="w-auto object-cover drop-shadow-xl opacity-10 lg:opacity-100"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="absolute lg:hidden bottom-0 right-0 flex items-center justify-center">
+    <img
+        src="/photo-full-downsized.png"
+        alt="portrait"
+        class="w-auto object-cover drop-shadow-xl opacity-10"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+</script>
