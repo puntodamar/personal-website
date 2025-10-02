6a6836ade5e60c846f5c298ed140f1886ad8cbb4
puntodamar <punto.damar2@gmail.com>
2025-10-02 21:39:45 +0700

    add: header class

--- app/components/headers/TheHeader.vue
+++ app/components/headers/TheHeader.vue
@@ -1,9 +1,8 @@
 <template>
-  <header class="absolute inset-x-0 top-0 z-50">
-
+  <header :class="headerClasses">
     <desktop-header :navigation="navigation" :title="appConfig.site.title" :logo="appConfig.site.nuxtLogo" />
     <mobile-header :navigation="navigation" :title="appConfig.site.title" :logo="appConfig.site.nuxtLogo"  />
-
+    <page-theme class="absolute top-6 right-20"/>
   </header>
 </template>
 <script setup>
@@ -11,13 +10,18 @@ import { ref } from 'vue'
 
 import MobileHeader from "~/components/headers/MobileHeader.vue";
 import DesktopHeader from "~/components/headers/DesktopHeader.vue";
-const appConfig = useAppConfig()
 
+const appConfig = useAppConfig()
+const props = defineProps({
+  absolute: {type: Boolean, default: true},
+})
+
+const headerClasses = computed(() => props.absolute ? 'inset-x-0 top-0 z-50 absolute' : 'inset-x-0 top-0 z-50' )
 const navigation = [
-  { name: 'About Me', href: '#' },
+  { name: 'About Me', href: '/' },
   { name: 'Experience', href: '#' },
   { name: 'Side Projects', href: '#' },
-  { name: 'Contact Me', href: '#' },
+  { name: 'Contact', href: '/contact' },
 ]
 
 const mobileMenuOpen = ref(false)
