161e68359efa33041dcf4ba800c0a48e877c5339
puntodamar <punto.damar2@gmail.com>
2025-10-06 19:02:14 +0700

    fix: dark mode on safari

--- nuxt.config.mjs
+++ nuxt.config.mjs
@@ -1,40 +1,48 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
+  vite: { plugins: [tailwindcss()] },
 
-    css: ["~/assets/css/main.css"],
-    vite: { plugins: [tailwindcss()] },
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-        head: {
-            bodyAttrs: {
-                class: 'bg-app',
-            },
-            link: [
-                { rel: "preconnect", href: "https://fonts.googleapis.com" },
-                { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
-                {
-                    rel: "stylesheet",
-                    href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-                }
-            ],
-            meta: [
-                // Helps browsers pick correct form controls, etc.
-                { name: "color-scheme", content: "light dark" }
-            ],
-            script: [
-                {
-                    innerHTML: `
-                          try {
-                          const ls = localStorage.getItem('color-mode');
-                           const wantDark = ls ? ls === 'dark' : false; // cookie handles SSR, LS handles first paint
-                           document.documentElement.classList.toggle('dark', wantDark);
-                          } catch (e) {}
-                        `,
-                    tagPosition: 'head'
-                }
-            ],
-            __dangerouslyDisableSanitizersByTagID: { },
-        }
-    },
+  app: {
+      pageTransition: { name: 'page', mode: 'out-in' },
+      colorMode: {
+          classSuffix: '',
+          preference: 'system',      //  fallback
+          fallback: 'light',         // SSR fallback
+          storageKey: 'color-theme'        // keep it in sync with localStorage/cookie
+      },
+      head: {
+          bodyAttrs: {
+              class: 'bg-app',
+          },
+          link: [
+              { rel: "preconnect", href: "https://fonts.googleapis.com" },
+              { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+              {
+                  rel: "stylesheet",
+                  href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+              }
+          ],
+          meta: [
+              // Helps browsers pick correct form controls, etc.
+              { name: "color-scheme", content: "light dark" }
+          ],
+          script: [
+              {
+                  innerHTML: `
+                        try {
+                        const ls = localStorage.getItem('color-mode');
+                         const wantDark = ls ? ls === 'dark' : false; // cookie handles SSR, LS handles first paint
+                         document.documentElement.classList.toggle('dark', wantDark);
+                        } catch (e) {}
+                      `,
+                  tagPosition: 'head'
+              }
+          ],
+          __dangerouslyDisableSanitizersByTagID: { },
+      }
+  },
+
+  modules: ["@nuxtjs/color-mode"],
 });
