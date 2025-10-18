d151e5a1742de5d6fcc00da9ac017489a8d59e76
puntodamar <punto.damar2@gmail.com>
2025-10-18 21:31:38 +0700

    add: vercel analytic

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,12 @@
 <template>
+  <Analytics />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup>
+import { Analytics } from '@vercel/analytics/nuxt';
 useHead({
   // htmlAttrs: { class: 'dark' },
   bodyAttrs: { class: 'antialiased' }
